9d8eac395d9a25772864987bf8886cdbc20e453c
Eduardo Rivera <ef.riveras@gmail.com>
2025-11-26 12:05:06 -0800

    forced push

--- update_enviroment_yml.py
+++ update_enviroment_yml.py
@@ -1,8 +1,8 @@
-"""Library of functions used to append package versions in environment.yml files
+"""Library of functions used to append package versions in environment.yml files 
 
-Example command line usage:
-python update_enviroment_yml.py --root_dir="." --env_name="ai_env" --yml_name="environment.yml"
-
+   Example command line usage: 
+   python scripts/00-update_enviroment_yml.py --root_dir="." --env_name="ai_env"
+   
 """
 
 import re
@@ -39,35 +39,6 @@ def process_dependency(dep, installed_packages):
             return dep  # keep as original if not installed
 
 
-def process_dependency_pip(dep, installed_packages):
-    """
-    For pip installed packages:
-
-    Processes a single dependency to update its version based on installed packages.
-
-    Parameters:
-        dep (str): The dependency string (e.g., "pandas=2.2.3" or "numpy").
-        installed_packages (dict): A dictionary of installed package names and versions. from "conda list"
-
-    Returns:
-        str: The updated dependency string with the version if found, or the original dependency.
-    """
-    # Check if the dependency has no version added
-    if "=" not in dep:
-        pkg_name = dep.strip()
-        if pkg_name in installed_packages:  # Update dependency if in the conda list
-            return f"{pkg_name}=={installed_packages[pkg_name]}"
-        else:
-            return dep  # keep as original if not installed
-    else:
-        pkg_name = re.split(r"=|>=|<=", dep)[0]
-        if pkg_name in installed_packages:
-            # still re-write the version even if there is a version installed already
-            return f"{pkg_name}=={installed_packages[pkg_name]}"
-        else:
-            return dep  # keep as original if not installed
-
-
 def update_environment_yml(env_name, yml_file="./environment.yml"):
     """
     Updates the `environment.yml` file with the correct package versions
@@ -99,7 +70,7 @@ def update_environment_yml(env_name, yml_file="./environment.yml"):
         if isinstance(dep, dict) and "pip" in dep:  # Process the 'pip' dictionary
             updated_pip_dependencies = []
             for pip_dep in dep.get("pip", []):  # process every pip dependencies
-                new_pip_dep = process_dependency_pip(pip_dep, installed_packages)
+                new_pip_dep = process_dependency(pip_dep, installed_packages)
                 updated_pip_dependencies.append(new_pip_dep)
             new_dep = dep
             new_dep["pip"] = updated_pip_dependencies  # update pip dictionary
@@ -114,7 +85,8 @@ def update_environment_yml(env_name, yml_file="./environment.yml"):
     env_data["dependencies"] = updated_dependencies
     with open(yml_file, "w") as f:
         yaml.dump(env_data, f, sort_keys=False)
-        print(f"Updated {yml_file} with versions from the {env_name} environment.")
+
+    print(f"Updated {yml_file} with versions from the {env_name} environment.")
 
 
 @click.command()
@@ -130,13 +102,7 @@ def update_environment_yml(env_name, yml_file="./environment.yml"):
     type=str,
     help="Name of your conda virtual environment. Default is `ai_env`",
 )
-@click.option(
-    "--yml_name",
-    default="environment.yml",
-    type=str,
-    help="Name of the yml file recording packages you installed in your conda environment. Default is `environment.yml`",
-)
-def main(root_dir, env_name, yml_name):
+def main(root_dir, env_name):
     """grab environment.yml, and add version numbers after each package"""
 
     # in case anyone just entered "" to indicate it's root directory
@@ -144,7 +110,7 @@ def main(root_dir, env_name, yml_name):
         root_dir = "."
 
     # using the functions
-    yml_path = Path(root_dir) / yml_name
+    yml_path = Path(root_dir) / "environment.yml"
     update_environment_yml(env_name, yml_path)
 
 
